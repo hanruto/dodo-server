43e980933501761861f8394154de268a9b600244
xiaoHan <qiaoming@zcool.com.cn>
2018-10-20 11:32:41 +0800

    d

--- deploy/index.js
+++ deploy/index.js
@@ -3,7 +3,7 @@ const childProcess = require('child_process'),
     { app } = require('../config/koa')
 
 const router = new Router()
-router.post('/deploy/:projectName', (ctx) => {
+router.post('/api/deploy/:projectName', (ctx) => {
     const { projectName } = ctx.params
     childProcess.exec(`sh /project/${projectName}/deploy.sh`)
     ctx.body = '部署成功'
