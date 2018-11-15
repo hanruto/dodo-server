d8afac48fb11c8684c52599de19348ec31a1577e
xiaoHan <qiaoming@zcool.com.cn>
2018-11-15 22:00:28 +0800

    f

--- modules/leaved-message/leaved-message.route.js
+++ modules/leaved-message/leaved-message.route.js
@@ -2,6 +2,6 @@ const leavedMessage = require('./leaved-message.controller'),
   { router } = require('../../config/koa'),
   { checkRole } = require('../../tools/acl')
 
-router.get('/leaved-messages', checkRole('admin'), leavedMessage.list)
-  .post('/leaved-messages', checkRole('admin'), leavedMessage.create)
+router.get('/leaved-messages', leavedMessage.list)
+  .post('/leaved-messages', leavedMessage.create)
   .delete('/leaved-messages/:id', checkRole('admin'), leavedMessage.delete)
