cd7a3fd0b42002e865e0b887b75d89c0e35ce324
qiaoming <qiaoming@guanghe.tv>
2019-10-20 21:08:53 +0800

    fix: 路由处理

--- modules/yc-h5/yc-h5.route.js
+++ modules/yc-h5/yc-h5.route.js
@@ -1,3 +1,5 @@
+const ycH5Controller = require('./yc-h5.controller')
+
 module.exports = router => {
-  router.get('/ych5/sentry-error')
+  router.get('/ych5/sentry-error', ycH5Controller.catchYCH5SentryError)
 }
